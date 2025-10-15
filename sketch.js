fe6d14f1a6ebba0a135416c3429f28d8b45a9c8d
Flo :) <103675778+FlorenceGayton@users.noreply.github.com>
2025-10-15 11:54:44 +0100

    Update sketch.js

---
+++ sketch.js
@@ -0,0 +1,56 @@
+const GRID_SIZE = 20;
+
+let headX = 200;
+let headY = 200;
+
+let xSpeed = GRID_SIZE;
+let ySpeed = 0;
+
+let foodX;
+let foodY;
+
+let isGameOver = false;
+
+function setup() {
+  createCanvas(600, 600);
+  placeFood();
+  frameRate(3);
+}
+
+function draw() {
+  background(220);
+
+  if (!isGameOver) {
+    headX += xSpeed;
+    headY += ySpeed;
+
+    if (
+      headX < 0 || headX >= width ||
+      headY < 0 || headY >= height
+    ) {
+      isGameOver = true;
+    }
+  }
+
+  fill(0, 255, 0);
+  rect(headX, headY, GRID_SIZE, GRID_SIZE);
+
+  fill(255, 0, 0);
+  ellipse(foodX + GRID_SIZE / 2, foodY + GRID_SIZE / 2, GRID_SIZE);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW || key === 'w') {
+    xSpeed = 0;
+    ySpeed = -GRID_SIZE;
+  } else if (keyCode === DOWN_ARROW || key === 's') {
+    xSpeed = 0;
+    ySpeed = GRID_SIZE;
+  } else if (keyCode === LEFT_ARROW || key === 'a') {
+    xSpeed = -GRID_SIZE;
+    ySpeed = 0;
+  } else if (keyCode === RIGHT_ARROW || key === 'd') {
+    xSpeed = GRID_SIZE;
+    ySpeed = 0;
+  }
+}
